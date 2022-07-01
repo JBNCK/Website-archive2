cf6696e995c9eec785bf9c29ede83cb4a36d6dda
Jan Biernacik <jan.biernacik@e.email>
2022-07-01 19:01:30 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function onload() {
+    placeholder_var = 0;
+    console.log("set all variables.")
+}
+$(window).scroll(function () {
+    if ($(window).scrollTop() >= 40) {
+        $('.navbar-desktop').css('background','black');
+        $('.navbar-desktop').css('opacity','90%');
+    } else {
+        $('.navbar-desktop').css('background','transparent');
+    }
+});

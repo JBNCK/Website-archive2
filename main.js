2ae1d84af39bbdf38ccfdc0da1311829c7f28be8
Jan Biernacik <jan.biernacik@e.email>
2022-07-03 14:02:19 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ function onload() {
     console.log("set all variables.")
 }
 $(window).scroll(function () {
-    if ($(window).scrollTop() >= 5) {
+    if ($(window).scrollTop() >= 10) {
         $('.navbar-desktop').css('background','black');
         $('.navbar-desktop').css('opacity','80%');
     } else {

43eee83a24ec9cad0f3064d7bf74d4b128c4036b
Jan Biernacik <spam@janbiernacik.com>
2023-01-28 16:19:59 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,50 +1,44 @@
-function redirect_home() {
-    window.location.href = "de/";
-}
-
 function onload() {
-    console.log("setting variables")
-    menu_expanded = 0;
-    var element = document.getElementById("menu_content");
-    element.style.opacity = "0";
+    placeholder_var = 0;
+    menu_var = 0;
+    console.log("set all variables.")
 }
-
 $(window).scroll(function () {
-    if ($(window).scrollTop() >= 1) {
-        $('.navbar').css('background','#191b24');
-        $('.navbar_links_current').css('background-color','darkslateblue');
-        $('.navbar_links_current').css('border','darkslateblue solid 10px');
-        $('.navbar_links_current').css('color','white');
-        $('.navbar').css('opacity','100%');
+    if ($(window).scrollTop() >= 8) {
+        $('.navbar-desktop').css('background','black');
+        $('.navbar-desktop').css('opacity','90%');
+        $('.navbar-mobile').css('background','black');
+        $('.navbar-mobile').css('opacity','90%');
     } else {
-        $('.navbar').css('background','transparent');
-        $('.navbar_links_current').css('background-color','white');
-        $('.navbar_links_current').css('border','white solid 10px');
-        $('.navbar_links_current').css('color','black');
+        $('.navbar-desktop').css('background','transparent');
+        $('.navbar-mobile').css('background','transparent');
     }
 });
-
 function menu() {
-    if (menu_expanded == 0) {
-        var element = document.getElementById("menu")
-        element.setAttribute("style", "height: 100vh;")
-        var element = document.getElementById("menu_content")
-        element.setAttribute("style", "height: 100vh;")
-        element.setAttribute("style", "visibility: visible;")
+    if (placeholder_var == 0) {
+        var element = document.getElementById("mobile-menu-id");
+        element.setAttribute("style", "padding: 10px;");
+        element.setAttribute("style", "padding-top: 240px;");
+        var element = document.getElementById("menu-content-id");
+        element.setAttribute("style", "top: -200px;")
         setTimeout( () =>{
-            element.style.opacity = "1";
+            element.setAttribute("style", "visibility: visible;")
         }, 250);
-        menu_expanded = 1;
+        placeholder_var = 1;
     }
     else {
-        var element = document.getElementById("menu_content")
-        element.style.opacity = "0";
-        element.setAttribute("style", "height: 0;")
-        element.setAttribute("style", "visibility: hidden;")
-        var element = document.getElementById("menu")
+        var element = document.getElementById("mobile-menu-id");
+        element.setAttribute("style", "padding: 0;");
+        element.setAttribute("style", "padding-top: 0;");
+        var element = document.getElementById("menu-content-id");
+        element.setAttribute("style", "top: -200px;")
         setTimeout( () =>{
-            element.setAttribute("style", "height: 0;")
-        }, 250);
-        menu_expanded = 0;
+            element.setAttribute("style", "display: none;")
+        }, 100);
+        placeholder_var = 0;
     }
+}
+function close_bottom_banner() {
+    var element = document.getElementById("bottom-banner-id");
+    element.setAttribute("style", "display: none;");
 }
